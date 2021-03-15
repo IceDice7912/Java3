a0fed23d7bc81ba3ddb9bb148a6cd4f1c97b0610
IceDice <dice7912@naver.com>
2021-03-15 12:09:22 +0900

    My semi #3

--- ProJect_이상묵/src/members/MemberDAO.java
+++ ProJect_이상묵/src/members/MemberDAO.java
@@ -1,12 +1,56 @@
 package members;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 public class MemberDAO {
+	private DataSource dataFactory;
+	private Connection conn;
+	private PreparedStatement pstmt;
+
+	public MemberDAO() {
+		try {
+			Context ctx = new InitialContext();
+			Context envContext = (Context) ctx.lookup("java:/comp/env");
+			dataFactory = (DataSource) envContext.lookup("jdbc/oracle");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	public List<MemberVO> listMembers() {
-		// TODO Auto-generated method stub
-		return null;
+		List<MemberVO> membersList = new ArrayList();
+		try {
+			conn = dataFactory.getConnection();
+			String query = "select * from member";
+			System.out.println(query);
+			pstmt = conn.prepareStatement(query);
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String pwd = rs.getString("pwd");
+				String name = rs.getString("name");
+				String sex = rs.getString("sex");
+				int age = rs.getInt("age");
+				MemberVO memberVO = new MemberVO(id, pwd, name, sex, age);
+				membersList.add(memberVO);
+			}
+			rs.close();
+			pstmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return membersList;
 	}
 
 }
--- ProJect_이상묵/src/members/MemberVO.java
+++ ProJect_이상묵/src/members/MemberVO.java
@@ -1,5 +1,70 @@
 package members;
 
-public class MemberVO {
+import java.sql.Date;
 
+public class MemberVO {
+	private String id;
+	private String pwd;
+	private String name;
+	private String sex;
+	private int age;
+	
+	
+	public MemberVO() {
+		System.out.println("MemberVO 생성자 호출");
+	}
+
+
+
+	public MemberVO(String id, String pwd, String name, String sex, int age) {
+		super();
+		this.id = id;
+		this.pwd = pwd;
+		this.name = name;
+		this.sex = sex;
+		this.age = age;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	
 }
